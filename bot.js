dbc5951c6720674be50a433ac31e179b16afe3fb
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2019-07-10 22:43:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2561,7 +2561,7 @@ client.on('message', msg => {
   command = command.slice(prefix.length);
   let args = msg.content.split(" ").slice(1);
 
-    if(command === "مسح") || if(command === "#clear"){
+    if(command === "مسح"){
         const emoji = client.emojis.find("name", "wastebasket")
     let textxt = args.slice(0).join("");
     if(msg.member.hasPermission("MANAGE_MESSAGES")) {
