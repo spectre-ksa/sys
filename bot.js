cd84de9a6d4a823416cf74e9176d3a2a6c2f8d4c
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2019-07-11 02:19:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3718,71 +3718,134 @@ client.on("message", msg => {
 
 
 
-let anti = JSON.parse(fs.readFileSync("./antigreff.json", "UTF8"));
-let config = JSON.parse(fs.readFileSync("./config.json", "UTF8"));
-client.on("message", message => {
-    if (!message.channel.guild) return;
-    let user = anti[message.guild.id + message.author.id]
-    let num = message.content.split(" ").slice(2).join(" ");
-    if (!anti[message.guild.id + message.author.id]) anti[message.guild.id + message.author.id] = {
-        actions: 0
-    }
-    if (!config[message.guild.id]) config[message.guild.id] = {
-        banLimit: 3,
-        chaDelLimit: 3,
-        roleDelLimit: 3,
-        kickLimits: 3,
-        roleCrLimits: 3,
-        time: 30
-    }
-    if (message.content.startsWith(prefix + "settings limits")) {
+client.on('message', async message => {
+if(!points) points = {}
 
-
-        if (!message.member.hasPermission('MANAGE_GUILD')) return;
-        if (message.content.startsWith(prefix + "settings limitsban")) {
-            if (!num) return message.channel.send("**⇏ | send a number ! ! **");
-            if (isNaN(num)) return message.channel.send("**⇏ | numbers only ! **");
-            config[message.guild.id].banLimit = num;
-            message.channel.send(`**⇏ | changed to : ${config[message.guild.id].banLimit} **`)
-        }
-        if (message.content.startsWith(prefix + "settings limitskick")) {
-                        if (!num) return message.channel.send("**⇏ | send a number ! ! **");
-                        if (isNaN(num)) return message.channel.send("**⇏ | numbers only ! **");
-            config[message.guild.id].kickLimits = num;
-            message.channel.send(`**⇏ | changed to : ${config[message.guild.id].kickLimits}**`)
-        }
-        if (message.content.startsWith(prefix + "settings limitsroleD")) {
-                        if (!num) return message.channel.send("**⇏ | send a number ! ! **");
-                        if (isNaN(num)) return message.channel.send("**⇏ | numbers only ! **");
-            config[message.guild.id].roleDelLimit = num;
-            message.channel.send(`**⇏ | changed to : ${config[message.guild.id].roleDelLimit}**`)
-        }
-        if (message.content.startsWith(prefix + "settings limitsroleC")) {
-                        if (!num) return message.channel.send("**⇏ | send a number ! ! **");
-                        if (isNaN(num)) return message.channel.send("**⇏ | numbers only ! **");
-            config[message.guild.id].roleCrLimits = num;
-            message.channel.send(`**⇏ | changed to : ${config[message.guild.id].roleCrLimits}**`)
-        }
-        if (message.content.startsWith(prefix + "settings limitschannelD")) {
-                        if (!num) return message.channel.send("**⇏ | send a number ! ! **");
-                        if (isNaN(num)) return message.channel.send("**⇏ | numbers only ! **");
-            config[message.guild.id].chaDelLimit = num;
-            message.channel.send(`**⇏ | changed to : ${config[message.guild.id].chaDelLimit}**`)
-        }
-        if (message.content.startsWith(prefix + "settings limitstime")) {
-                        if (!num) return message.channel.send("**⇏ | send a number ! ! **");
-                        if (isNaN(num)) return message.channel.send("**⇏ | numbers only ! **");
-            config[message.guild.id].time = num;
-            message.channel.send(`**⇏ | changed to : ${config[message.guild.id].time}**`)
-        }
-        fs.writeFile("./config.json", JSON.stringify(config, null, 2), function (e) {
-            if (e) throw e;
-        });
-        fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function (e) {
-            if (e) throw e;
-        });
-    }
+	if(message.channel.type !== 'text') return;
+	
+	
+	var command = message.content.toLowerCase().split(" ")[0];
+	var args = message.content.toLowerCase().split(" ");
+	var userM = message.guild.member(message.mentions.users.first() || message.guild.members.find(m => m.id == args[1]));
+	  const embed  = new Discord.RichEmbed()
+.setDescription(`
+**لم يتم تسجيل أي نقطة حتى الأن **
+** أمثلة للأوامر: **
+**:small_orange_diamond:** .points ${message.author} 1 \`لتغيير نقاط شخص معين \`
+**:small_orange_diamond:** .points ${message.author} +1 \`لزيادة نقاط شخص معين\`
+**:small_orange_diamond:** .points ${message.author} -1 \`لأزالة نقطة من شخص معين \`
+**:small_orange_diamond:** .points ${message.author} 0 \`لتصفير نقاط شخص معين \`
+**:small_orange_diamond:** .points reset \`لتصفير جميع النقاط\``)
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setColor(`#e60909`)
+  const error  = new Discord.RichEmbed()
+.setDescription(`
+**:x: | يجب كتابة الأمر بشكل صحيح. **
+** أمثلة للأوامر: **
+**:small_orange_diamond:** .points ${message.author} 1 \`لتغيير نقاط شخص معين \`
+**:small_orange_diamond:** .points ${message.author} +1 \`لزيادة نقاط شخص معين\`
+**:small_orange_diamond:** .points ${message.author} -1 \`لأزالة نقطة من شخص معين \`
+**:small_orange_diamond:** .points ${message.author} 0 \`لتصفير نقاط شخص معين \`
+**:small_orange_diamond:** .points reset \`لتصفير جميع النقاط\``)
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setColor(`#e60909`)
+if(command == prefix + 'points') {
+	 
+		if(!message.guild.member(client.user).hasPermission('EMBED_LINKS')) return message.channel.send(':no_entry: | I dont have Embed Links permission.');
+		if(!args[1]) {
+			if(!points) return message.channel.send(embed);
+			var members = Object.values(points);
+			var memb = members.filter(m => m.points >= 1);
+			if(memb.length == 0) return message.channel.send(embed);
+			var x = 1;
+			let pointsTop = new Discord.RichEmbed()
+			.setAuthor('Points:')
+			.setColor('#FBFBFB')
+			.setDescription(memb.sort((second, first) => first.points > second.points).slice(0, 10).map(m => `**:small_blue_diamond:** <@${m.id}> \`${m.points}\``).join('\n'))
+			.setFooter(`Requested by ${message.author.username}`, message.author.avatarURL);
+			message.channel.send({
+				embed: pointsTop
+			});
+		}else if(args[1] == 'reset') {
+			let pointsReset = new Discord.RichEmbed()
+			.setDescription('**:white_check_mark: | تم تصفير جميع النقاط بنجاح**')
+			.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+			if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send("You dont have Manage Server permission.");
+			if(!points) return message.channel.send(pointsReset);
+			var members = Object.values(points);
+			var memb = members.filter(m => m.points >= 1);
+			if(memb.length == 0) return message.channel.send(pointsReset);
+			points = {};
+			message.channel.send(pointsReset);
+		}else if(userM) {
+			if(!message.member.hasPermission('MANAGE_GUILD')) return  message.channel.send("You dont have Manage Server permission.");
+			if(!points[userM.user.id]) points[userM.user.id] = {
+				points: 0,
+				id: userM.user.id
+			};
+			if(!args[2]) {
+				if(points[userM.user.id].points == 0) return message.channel.send( `${userM.user.username} Not have any points.`);
+				var userPoints = new Discord.RichEmbed()
+				.setColor('#d3c325')
+				.setAuthor(`${userM.user.username} have ${points[userM.user.id].points} points.`);
+				message.channel.send({
+					embed: userPoints
+				});
+			}else if(args[2] == 'reset') {
+				if(points[userM.user.id].points == 0) return message.channel.send(error);
+				points[userM.user.id].points = 0;
+				message.channel.send(`Successfully reset ${userM.user.username} points.`);
+			}else if(args[2].startsWith('+')) {
+				args[2] = args[2].slice(1);
+				args[2] = parseInt(Math.floor(args[2]));
+				if(points[userM.user.id].points == 1000000) return message.channel.send(error);
+				if(!args[2]) return message.channel.send(error);
+				if(isNaN(args[2])) return message.channel.send(error);
+				if(args[2] > 1000000) return message.channel.send(error);
+				if(args[2] < 1) return message.channel.send(error);
+				if((points[userM.user.id].points + args[2]) > 1000000) args[2] = 1000000 - points[userM.user.id].points;
+				points[userM.user.id].points += args[2];
+				let add = new Discord.RichEmbed()
+				.setDescription(`**:small_blue_diamond:** <@${userM.id}> \`${points[userM.user.id].points}\``)
+				.setAuthor('Points:')
+				.setColor('#FBFBFB')
+				.setFooter('Requested by' + message.author.username, message.author.avatarURL)
+				message.channel.send(add);
+			}else if(args[2].startsWith('-')) {
+				args[2] = args[2].slice(1);
+				args[2] = parseInt(Math.floor(args[2]));
+				if(points[userM.user.id].points == 0) return message.channel.send(error);
+				if(!args[2]) return message.channel.send(error);
+				if(isNaN(args[2])) return message.channel.send(error);
+				if(args[2] > 1000000) return message.channel.send(error);
+				if(args[2] < 1) return message.channel.send(error);
+				if((points[userM.user.id].points - args[2]) < 0) args[2] = points[userM.user.id].points;
+				points[userM.user.id].points -= args[2];
+					let rem = new Discord.RichEmbed()
+				.setDescription(`**:small_blue_diamond:** <@${userM.id}> \`${points[userM.user.id].points}\``)
+				.setAuthor('Points:')
+				.setColor('#FBFBFB')
+				.setFooter('Requested by' + message.author.username, message.author.avatarURL)
+				message.channel.send(rem);
+			}else if(!args[2].startsWith('+') || !args[2].startsWith('-')) {
+				args[2] = parseInt(Math.floor(args[2]));
+				if(isNaN(args[2])) return message.channel.send(error);
+				if(args[2] > 1000000) return message.channel.send(error);
+				if(args[2] < 1) return message.channel.send(error);
+				if(points[userM.user.id].points == args[2]) return message.channel.send(`${userM.user.username} points is already ${args[2]}.`);
+				points[userM.user.id].points = args[2];
+					let set = new Discord.RichEmbed()
+				.setDescription(`**:small_blue_diamond:** <@${userM.id}> \`${points[userM.user.id].points}\``)
+				.setAuthor('Points:')
+				.setColor('#FBFBFB')
+				.setFooter('Requested by' + message.author.username, message.author.avatarURL)
+				message.channel.send(set);
+			}
+			}
+			}
+  
 });
+
 
 
 
