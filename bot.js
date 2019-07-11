669a350a1a2cc11abf0b1b1fbf598516a3e4a22a
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2019-07-11 12:08:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4252,7 +4252,6 @@ message.channel.send(`Please wait until the order is finished`).then(msg =>{
       var msg = message.content.toLowerCase();
       if( !message.guild ) return;
       var prefix = "#"
-      let args = message.content.split(' ').slice(1);
       if(message.content.startsWith(prefix + '-role')) {
           if(!message.member.hasPermission('MANAGE_ROLES')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `MANAGE_ROLES`' );
       let member = message.mentions.users.first();
